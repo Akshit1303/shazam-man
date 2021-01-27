053832393cd8d4ba9c3b679f25eecefd2ce6bc13
Akshit1303 <akshit.pathania619@gmail.com>
2021-01-26 19:49:43 -0500

    first checkin of initial objects
    Describing songs, artist, and activities.

---
+++ ShazamExample1/ShazamExample1/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShazamExample1
+{
+    class Song
+    {
+        private string _SongTitle;
+        private DateTime _ReleaseDate;
+        private Artist _Artist;
+        private float _Duration;
+
+        public string SongTitle { get => _SongTitle; set => _SongTitle = value; }
+        
+        public float Duration { get => _Duration; set => _Duration = value; }
+        internal Artist Artist { get => _Artist; set => _Artist = value; }
+    }
+}
